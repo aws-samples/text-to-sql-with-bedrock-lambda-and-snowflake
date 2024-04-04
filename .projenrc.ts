4ee7e256e9b3c59a034ad643d8e7ed1d94eaba4c
Galen Dunkleberger <awsgalen@amazon.com>
2024-04-04 13:48:47 -0400

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,14 @@
+import { awscdk, javascript } from 'projen';
+const project = new awscdk.AwsCdkTypeScriptApp({
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'text-to-sql-with-athena-and-snowflake',
+  packageManager: javascript.NodePackageManager.PNPM,
+  projenrcTs: true,
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
